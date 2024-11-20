e81e254ed9679bf6f596a043f8ad3b2d6e2d2728
Gourav Giri Goswami <gouravgiri2004@gmail.com>
2024-11-20 07:24:05 +0000

    variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const id = 123;
+let emailID = "gouravgiri2004@gmail.com";
+var accountPass = "12345";
+let accountstate;
+console.log(id);
+//id = 12; error const variable is not changed
+
+emailID = "gouravgiri2654@gmail.com";//let is changed
+
+accountPass = "2343"; //var is also changed
+
+name = "Gourav";
+
+name = "xyz";
+
+console.table([name,id,emailID,accountPass,accountstate]);
+
+{
+    //scope
+}
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+//alert(3+3); // error beacause it run in node
+
+// console.log(3+3); 
+
+// console.log("Gourav");
+
+let name ="Gourav";
+let age = 20;
+let islogin = true;
+let state = null;
+console.log(name +" "+ typeof name);
+
+console.table([name+" "+typeof name,age+" "+typeof age]);
+
+//number
+// bigint
+//string
+// Boolean
+// null
+// undefined

b398154de6dd288e2a85c6110bb50a3a74caa7d5
Gourav Giri Goswami <gouravgiri2004@gmail.com>
2024-11-20 07:35:39 +0000

    Conversions

---
+++ 01_basics/03_datatypeConversion.js
@@ -0,0 +1,15 @@
+let score =33;
+
+console.log(typeof score);
+
+let valinstring ="333";
+console.log(typeof valinstring);
+
+if(valinstring===String) console.log("true");
+else console.log("false");
+
+let valinnum = Number(valinstring); //type conversion
+console.log(typeof valinnum);
+
+if(valinnum===Number) console.log("true");
+else console.log("false");

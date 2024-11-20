ac59d896b338519cad7c18e8387d8a911f9cfd77
Gourav Giri Goswami <gouravgiri2004@gmail.com>
2024-11-20 09:41:27 +0000

    String

---
+++ 01_basics/05_String.js
@@ -0,0 +1,16 @@
+const name = "Gourav";
+const sarname = "Giri";
+
+console.log(name+" "+sarname);
+
+console.log(`Hello my name is ${name} and my sarname is ${sarname}`);
+
+const gamename = new String("xyz");
+console.log(gamename);
+console.log(gamename[0]);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+
+const newString = new String("Gourav");
+
+console.log(newString.substring(2,4));

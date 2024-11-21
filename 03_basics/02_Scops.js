e08102be9bc009807015c44b8c562ef7215f8095
Gourav Giri Goswami <gouravgiri2004@gmail.com>
2024-11-21 09:53:15 +0000

    Scops and ArrowFunctions

---
+++ 03_basics/02_Scops.js
@@ -0,0 +1,55 @@
+// let a=10;
+// const b=20;
+// var c=30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//scope--------
+// {
+//     let a=10;
+//     const b=20;
+//     var c=30;
+// }
+
+// //console.log(a);
+// //console.log(b);
+// console.log(c);
+
+// function one()
+// { 
+//     const usnername ="Gourav";
+    
+//     function two()
+//     {
+//         const website ="youtube";
+//         console.log(usnername);
+        
+//     }
+//     //console.log(website);
+
+//     two();
+// }
+// one();
+
+
+if(true)
+{
+    const username ="Gourav";
+    
+    if(username==="Gourav")
+    {
+        const website="youtube";
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+const nameofperson = function name(hii)
+{
+    return "Gourav";
+}
+console.log(nameofperson(4));
---
+++ 03_basics/03_ArrowFunction.js
@@ -0,0 +1,27 @@
+// const user ={
+//     username:"Gourav",
+//     price:99,
+//     welcomeMessage:function(){
+//         console.log(`${this.username} , welcome to website `);
+        
+//     }
+// }
+
+// user.welcomeMessage();
+
+//....................ArrowFunction.......................
+
+// const chai = () =>{
+//     let username ="Gourav Giri";
+//     console.log(this.username); 
+//     console.log(this);
+     
+// }
+// chai();
+
+const chai=(num1,num2)=>{
+return num1+num2;
+}
+
+console.log(chai(1,2));
+;

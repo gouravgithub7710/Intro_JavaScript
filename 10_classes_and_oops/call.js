2ac10cc352f9a42450b0ab412757329e1c51316f
Gourav Giri Goswami <gouravgiri2004@gmail.com>
2024-12-09 09:31:37 +0000

    call

---
+++ 10_classes_and_oops/call.js
@@ -0,0 +1,15 @@
+function setUsername (username)
+    {
+        //complex db calls
+        this.username=username;
+    }
+function createUser(username,email,password){
+    setUsername.call(this.susername)
+this.email=email;
+this.password=password;
+
+}
+
+const chai= new createUser("chai","chai@gmail.com",1234)
+
+console.log(chai);
